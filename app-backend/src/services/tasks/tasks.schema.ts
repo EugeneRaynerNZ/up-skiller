b98cd074f67ca2fec081aad90f78e9022174e4b7
Shuai Wang <wsking233@gmail.com>
2024-07-20 19:59:35 +1200

    Update tasks.schema.ts

--- app-backend/src/services/tasks/tasks.schema.ts
+++ app-backend/src/services/tasks/tasks.schema.ts
@@ -1,53 +1,75 @@
 // // For more information about this file see https://dove.feathersjs.com/guides/cli/service.schemas.html
 import { resolve } from '@feathersjs/schema'
-import { Type, getValidator, querySyntax } from '@feathersjs/typebox'
-import { ObjectIdSchema } from '@feathersjs/typebox'
-import type { Static } from '@feathersjs/typebox'
-
-import type { HookContext } from '../../declarations'
+import {
+  Type,
+  getValidator,
+  querySyntax,
+  ObjectIdSchema,
+} from '@feathersjs/typebox'
 import { dataValidator, queryValidator } from '../../validators'
+import { SkillStatusEnum } from '../enum'
+
+import type { Static } from '@feathersjs/typebox'
+import type { HookContext } from '../../declarations'
 import type { TasksService } from './tasks.class'
 
 // Main data model schema
 export const tasksSchema = Type.Object(
   {
     _id: ObjectIdSchema(),
-    text: Type.String()
+    topicId: ObjectIdSchema(),
+    name: Type.String(),
+    description: Type.Optional(Type.String()),
+    tags: Type.Optional(Type.Array(Type.String())),
+    status: SkillStatusEnum,
+    priority: Type.Optional(Type.Number()), // Task priority: 1-3
+    completedAt: Type.Optional(Type.Date()),
   },
-  { $id: 'Tasks', additionalProperties: false }
+  { $id: 'Tasks', timestamps: true, additionalProperties: false },
 )
 export type Tasks = Static<typeof tasksSchema>
 export const tasksValidator = getValidator(tasksSchema, dataValidator)
 export const tasksResolver = resolve<Tasks, HookContext<TasksService>>({})
 
-export const tasksExternalResolver = resolve<Tasks, HookContext<TasksService>>({})
+export const tasksExternalResolver = resolve<Tasks, HookContext<TasksService>>(
+  {},
+)
 
 // Schema for creating new entries
-export const tasksDataSchema = Type.Pick(tasksSchema, ['text'], {
-  $id: 'TasksData'
+export const tasksDataSchema = Type.Pick(tasksSchema, ['topicId', 'name','status'], {
+  $id: 'TasksData',
 })
 export type TasksData = Static<typeof tasksDataSchema>
 export const tasksDataValidator = getValidator(tasksDataSchema, dataValidator)
-export const tasksDataResolver = resolve<Tasks, HookContext<TasksService>>({})
+export const tasksDataResolver = resolve<Tasks, HookContext<TasksService>>({
+  // give status a default value
+  status: () => SkillStatusEnum.ACTIVE,
+})
 
 // Schema for updating existing entries
 export const tasksPatchSchema = Type.Partial(tasksSchema, {
-  $id: 'TasksPatch'
+  $id: 'TasksPatch',
 })
 export type TasksPatch = Static<typeof tasksPatchSchema>
 export const tasksPatchValidator = getValidator(tasksPatchSchema, dataValidator)
 export const tasksPatchResolver = resolve<Tasks, HookContext<TasksService>>({})
 
 // Schema for allowed query properties
-export const tasksQueryProperties = Type.Pick(tasksSchema, ['_id', 'text'])
+export const tasksQueryProperties = Type.Pick(tasksSchema, ['_id', 'topicId', 'name'])
 export const tasksQuerySchema = Type.Intersect(
   [
     querySyntax(tasksQueryProperties),
     // Add additional query properties here
-    Type.Object({}, { additionalProperties: false })
+    Type.Object({}, { additionalProperties: false }),
   ],
-  { additionalProperties: false }
+  { additionalProperties: false },
 )
 export type TasksQuery = Static<typeof tasksQuerySchema>
-export const tasksQueryValidator = getValidator(tasksQuerySchema, queryValidator)
-export const tasksQueryResolver = resolve<TasksQuery, HookContext<TasksService>>({})
+export const tasksQueryValidator = getValidator(
+  tasksQuerySchema,
+  queryValidator,
+)
+export const tasksQueryResolver = resolve<
+  TasksQuery,
+  HookContext<TasksService>
+>({})
